5456f6ae2c88841f996f45ae8e0c3a86adcb1374
shivanishede <shivanishede69@gmail.com>
2024-08-20 11:36:03 +0530

    Removed from master

--- myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# from .models import Donation
-
-from .models import DonarInfo, User
-
-admin.site.register(DonarInfo)
-admin.site.register(User)
--- myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import DonarInfo
-
-class DonarInfoForm(forms.ModelForm):
-    class Meta:
-        model = DonarInfo
-        fields = '__all__'
